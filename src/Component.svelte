d1ef2a43c3e2d76b43172761289b1beb9e73c9d3
edsonpixel <edson.bandola@pixel.ao>
2023-03-14 10:17:08 +0100

    feat: add calendar

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,12 +1,23 @@
 <script>
   import { getContext } from "svelte"
-
+  import Calendar from '@event-calendar/core';
+  import TimeGrid from '@event-calendar/time-grid';
   export let text
 
-  const { styleable } = getContext("sdk")
+  const { styleable } = getContext("sdk") 
   const component = getContext("component")
+
+  let plugins = [TimeGrid];
+  let options = {
+    view: 'timeGridWeek',
+    events: [
+        // your list of events
+    ]
+  };
+
 </script>
 
 <div use:styleable={$component.styles}>
   This is a custom component. The text setting is: {text}.
+  <Calendar {plugins} {options} />
 </div>
