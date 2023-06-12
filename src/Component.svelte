e3cfbff737da6d6dbf5165e5fb4179420499a997
Olivier@Diogenes <olivier@diogenes.brussels>
2023-06-12 15:28:49 +0200

    Code formatting

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,77 +1,88 @@
 <script>
-  
-  import { getContext } from "svelte"	
-  import '@fullcalendar/core/locales-all'
-  import FullCalendar from 'svelte-fullcalendar';
-  import daygridPlugin from '@fullcalendar/daygrid';
-  import listPlugin from '@fullcalendar/list';
+  import { getContext } from "svelte";
+  import "@fullcalendar/core/locales-all";
+  import FullCalendar from "svelte-fullcalendar";
+  import daygridPlugin from "@fullcalendar/daygrid";
+  import listPlugin from "@fullcalendar/list";
   import { onMount } from "svelte";
-  import {langs, codeLang} from "./lang"
- 
-  export let language
-  export let calendarEvent
-  
-  export let mappingTitle
-  export let mappingDate
-  export let mappingId
- 
-  export let mappingTitle2
-  export let mappingDate2
-  export let mappingId2
+  import { langs, codeLang } from "./lang";
 
-  export let dataProvider
-  export let dataProvider2
+  export let language;
+  export let calendarEvent;
 
-  export let mappingColor
-  export let mappingColor2
+  export let mappingTitle;
+  export let mappingDate;
+  export let mappingId;
 
-  export let allday
-  export let allday2
-  
-  let eventsList = []
-  onMount(()=>{
-    
-    if(eventsList.length > 0){
-      eventsList = []
+  export let mappingTitle2;
+  export let mappingDate2;
+  export let mappingId2;
+
+  export let dataProvider;
+  export let dataProvider2;
+
+  export let mappingColor;
+  export let mappingColor2;
+
+  export let allday;
+  export let allday2;
+
+  let eventsList = [];
+  onMount(() => {
+    if (eventsList.length > 0) {
+      eventsList = [];
     }
-    if(dataProvider.rows){
-      dataProvider.rows.forEach(event => {
-        let eventColor = mappingColor ?? '#313131'           
-        eventsList.push({ title: event[mappingTitle], date: event[mappingDate], id: event[mappingId], color: eventColor, event: event, allDay: allday   })        
+    if (dataProvider.rows) {
+      dataProvider.rows.forEach((event) => {
+        let eventColor = mappingColor ?? "#313131";
+        eventsList.push({
+          title: event[mappingTitle],
+          date: event[mappingDate],
+          id: event[mappingId],
+          color: eventColor,
+          event: event,
+          allDay: allday,
+        });
       });
     }
-    if(dataProvider2.rows){
-      dataProvider2.rows.forEach(event => {
-        let eventColor2 = mappingColor2 ?? '#eb4034' 
-        eventsList.push({ title: event[mappingTitle2], date: event[mappingDate2], id: event[mappingId2], color: eventColor2, event: event, allDay: allday2  })
+    if (dataProvider2.rows) {
+      dataProvider2.rows.forEach((event) => {
+        let eventColor2 = mappingColor2 ?? "#eb4034";
+        eventsList.push({
+          title: event[mappingTitle2],
+          date: event[mappingDate2],
+          id: event[mappingId2],
+          color: eventColor2,
+          event: event,
+          allDay: allday2,
+        });
       });
     }
-    eventsList = eventsList
-  })
+    eventsList = eventsList;
+  });
 
-  let options  = {
+  let options = {
     headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,dayGridWeek,dayGridDay'
+      left: "prev,next today",
+      center: "title",
+      right: "dayGridMonth,dayGridWeek,dayGridDay",
     },
     plugins: [daygridPlugin, listPlugin],
-    initialDate:  Date.now(),
+    initialDate: Date.now(),
     locale: language,
     dayMaxEvents: true,
-    eventClick: (event)=>{
+    eventClick: (event) => {
       calendarEvent({
-        value: event.event.id
-      })
+        value: event.event.id,
+      });
     },
-    events:eventsList,
-    eventColor: '#378006',
+    events: eventsList,
+    eventColor: "#378006",
     theme: true,
-    ...langs[codeLang(language)]
-  }
-  const { styleable } = getContext("sdk") 
-  const component = getContext("component")
-
+    ...langs[codeLang(language)],
+  };
+  const { styleable } = getContext("sdk");
+  const component = getContext("component");
 </script>
 
 <div use:styleable={$component.styles}>
